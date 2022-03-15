3ac803908c618e2bef2b7fd126160df0aba33c01
jgiven100 <joelgiven@berkeley.edu>
2022-03-15 15:55:15 -0700

    :bug: update to global min and max values

--- 2d/sliding_block_inclined_boundary/test_benchmark.py
+++ 2d/sliding_block_inclined_boundary/test_benchmark.py
@@ -10,12 +10,12 @@ df1 = pd.read_hdf('results/boundary_inclined_friction/particles-1_4-099000.h5', 
 df2 = pd.read_hdf('results/boundary_inclined_friction/particles-2_4-099000.h5', 'table')
 df3 = pd.read_hdf('results/boundary_inclined_friction/particles-3_4-099000.h5', 'table')
 df = df0
-df.append(df1)
-df.append(df2)
-df.append(df3)
+df = df.append(df1)
+df = df.append(df2)
+df = df.append(df3)
 
-## Assert location of particles
+# Assert location of particles
 assert round(df['coord_x'].min() - 3.177497667722451, 8) == 0.0
-assert round(df['coord_x'].max() - 4.008388126337369, 8) == 0.0
-assert round(df['coord_y'].min() - (-1.8296901482239594), 8) == 0.0
-assert round(df['coord_y'].max() - (-1.2655507481267576), 8) == 0.0
+assert round(df['coord_x'].max() - 4.458142966806158, 8) == 0.0
+assert round(df['coord_y'].min() - (-2.2671931421737326), 8) == 0.0
+assert round(df['coord_y'].max() - (-0.986546553344812), 8) == 0.0
